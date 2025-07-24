7d3b602c50faee528d3c278221974467f4a922ac
bolsakova <bolsakova@github.com>
2025-07-24 23:25:23 +0200

    finished ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -8,11 +8,16 @@ int main() {
 	std::string& stringREF = string;
 	// вывести адреса
 	//		1. адрес переменной - &string
-	//		2. адрес в указателе - stringPTR
-	//		3. адрес ссылки - &stringREF
-	
+	//		2. адрес в указателе - stringPTR (указатель уже содержит адрес, поэтому & не нужно)
+	//		3. адрес ссылки - &stringREF (равен адресу оригинальной переменной)
+	std::cout << "Адрес переменной string: " << &string << std::endl;
+	std::cout << "Адрес в указателе stringPTR: " << stringPTR << std::endl;
+	std::cout << "Адрес ссылки stringREF: " << &stringREF << std::endl;
 	// вывести значения
 	//		1. значение переменной - string
 	//		2. значение по указателю - *stringPTR
-	//		3. значение ссылки - &stringREF
+	//		3. значение ссылки - stringREF
+	std::cout << "Значение переменной string: " << string << std::endl;
+	std::cout << "Значение по указателю *stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Значение ссылки stringREF: " << stringREF << std::endl;
 }
