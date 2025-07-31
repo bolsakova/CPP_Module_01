575d23ae30d6af7b72d4cdb0d15fd924511ca53d
bolsakova <bolsakova@github.com>
2025-07-31 19:01:36 +0200

    ex06: improved main function for testing new method

--- ex06/main.cpp
+++ ex06/main.cpp
@@ -2,22 +2,14 @@
 
 // создать массив указателей на методы и массив строк уровней,
 // затем найти соответствие
-int main() {
+int main(int ac, char *av[]) {
+	if (ac != 2) {
+		std::cout << "Usage: ./harlFilter <level>" << std::endl;
+		return 1;
+	}
+	
 	Harl harl;
-
-	std::cout << "=== Testing Harl complaints ===" << std::endl << std::endl;
-
-	harl.complain("DEBUG");
-	std::cout << std::endl;
-	harl.complain("INFO");
-	std::cout << std::endl;
-	harl.complain("WARNING");
-	std::cout << std::endl;
-	harl.complain("ERROR");
-	std::cout << std::endl;
-
-	// тест неизвестного уровня
-	harl.complain("UNKNOWN");
+	harl.filter(av[1]);
 
 	return 0;
 }
